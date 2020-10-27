845ef4bdb95747463cc99f11e9e8567f2e8d0d02
contact <contact@artpologabriel.com>
2020-10-26 17:00:24 -0700

    up

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://3.20.189.143/src-api', {
+mongoose.connect('mongodb://3.20.189.143:27017/src-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
