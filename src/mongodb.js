3138a9e09714b92353d9fca419db666192523cd6
contact <contact@artpologabriel.com>
2020-10-26 23:32:22 -0700

    up

--- src/mongodb.js
+++ src/mongodb.js
@@ -2,7 +2,6 @@
 
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
-const add = require('./addfile/add')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -14,40 +13,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     
- 
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 28
-    //     }, {
-    //         name: 'Gunther',
-    //         age: 27
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents!')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Clean the house',
-    //         completed: true
-    //     },{
-    //         description: 'Renew inspection',
-    //         completed: false
-    //     },{
-    //         description: 'Pot plants',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert tasks!')
-    //     }
-
-    //     console.log(result.ops)
-    // })
+    db.collection('users').insertOne({
+        name: 'Andrew',
+        age: 27
+    })
 })
