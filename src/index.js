a4b3ef7a38287cf2f828f1f14496a2248c8a2224
contact <contact@artpologabriel.com>
2020-10-30 20:29:23 -0700

    up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
     res.render('hello world')
 })
