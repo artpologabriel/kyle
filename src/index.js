e4be2b94dde2e3df24d636297f2881f3be39bf5a
contact <contact@artpologabriel.com>
2020-10-30 20:16:47 -0700

    up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+
 
 const app = express()
 const port = process.env.PORT || 3000
